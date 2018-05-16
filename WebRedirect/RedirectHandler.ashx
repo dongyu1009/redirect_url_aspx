016ae2f759d53ab6e7628cee5f9ece4d5e693fce
dongyu <dongyu1009@163.com>
2018-05-16 23:58:26 +0800

    init

---
+++ WebRedirect/RedirectHandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="RedirectHandler.ashx.cs" Class="WebRedirect.RedirectHandler" %>
---
+++ WebRedirect/RedirectHandler.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace WebRedirect
+{
+    /// <summary>
+    /// RedirectHandler 的摘要说明
+    /// </summary>
+    public class RedirectHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string Query = context.Request.Url.Query.ToString();
+            string redirectURL = "http://127.0.0.1:8100/";
+            var request = (HttpWebRequest)WebRequest.Create(redirectURL + Query);
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(responseString);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
